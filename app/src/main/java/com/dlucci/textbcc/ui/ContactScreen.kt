e644deac96e612e073e1bb86455c8b157a2bfeb4
Derril Lucci <derril@Derrils-Laptop.local>
2026-03-09 09:43:25 -0400

    Adding search functionality

--- app/src/main/java/com/dlucci/textbcc/ui/ContactScreen.kt
+++ app/src/main/java/com/dlucci/textbcc/ui/ContactScreen.kt
@@ -1,19 +1,25 @@
 package com.dlucci.textbcc.ui
 
-import android.content.Context
-import android.provider.ContactsContract
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material3.Button
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -40,77 +46,70 @@ fun ContactScreen(navController: NavController, viewModel: TextBccViewModel = ko
     var contacts by remember { mutableStateOf<List<ContactNumber>>(emptyList()) }
     LaunchedEffect(Unit) {
         contacts = withContext(Dispatchers.IO) {
-            getContacts(context)
+            viewModel.getContacts(context)
         }
     }
+    var contactList by remember { mutableStateOf<List<ContactNumber>>(emptyList()) }
     val selectedContacts = remember { mutableStateListOf<ContactNumber>() }
-    Box(contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()) {
-        LazyColumn(modifier = Modifier.selectableGroup().align(Alignment.Center)) {
-            items(contacts) {
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(5.dp),
-                    modifier = Modifier.selectable(
-                        selected = selectedContacts.contains(it),
-                        onClick = {
-                            if (selectedContacts.contains(it))
-                                selectedContacts.remove(it)
-                            else
-                                selectedContacts.add(it)
-                        },
-                        role = Role.Checkbox
-                    )
-                ) {
-                    RadioButton(
-                        selected = selectedContacts.contains(it),
-                        onClick = null
-                    )
-                    Text(text = it.name)
-                    Text(text = it.phoneNumber)
+    LaunchedEffect(contacts) {
+        contactList = contacts
+    }
+    Scaffold(topBar = {SearchBar(contacts, onQueryChanged = {
+        filteredContacts ->
+           contactList = filteredContacts
+    })}) { innerPadding ->
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            LazyColumn(modifier = Modifier.selectableGroup()) {
+                items(contactList) {
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(5.dp),
+                        modifier = Modifier.selectable(
+                            selected = selectedContacts.contains(it),
+                            onClick = {
+                                if (selectedContacts.contains(it))
+                                    selectedContacts.remove(it)
+                                else
+                                    selectedContacts.add(it)
+                            },
+                            role = Role.Checkbox
+                        )
+                    ) {
+                        RadioButton(
+                            selected = selectedContacts.contains(it),
+                            onClick = null
+                        )
+                        Text(text = it.name)
+                        Text(text = it.phoneNumber)
+                    }
                 }
             }
-        }
-        Button(enabled = selectedContacts.isNotEmpty(), onClick = {
-            viewModel.inputUsers(selectedContacts)
-            navController.navigate(Screen.ComposeScreen.route)
-        }, modifier = Modifier.align(Alignment.BottomCenter).padding(bottom = 16.dp)) {
-            Text(text = "Next")
+            Button(enabled = selectedContacts.isNotEmpty(), onClick = {
+                viewModel.updateSelectedUsers(selectedContacts)
+                navController.navigate(Screen.ComposeScreen.route)
+            }, modifier = Modifier.align(Alignment.BottomCenter).padding(bottom = 16.dp)) {
+                Text(text = "Next")
+            }
         }
     }
 }
 
-/**
- * TODO:  move to viewmodel
- */
-private fun getContacts(context: Context): MutableList<ContactNumber> {
-    val contacts = mutableListOf<ContactNumber>()
-    val contentResolver = context.contentResolver
-    val cursor = contentResolver.query(
-        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-        arrayOf(
-            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-            ContactsContract.CommonDataKinds.Phone.NUMBER
-        ),
-        null,
-        null,
-        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME_PRIMARY + " ASC"
-    )
-    cursor.use {
-        val nameIndex =
-            it?.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME) as Int
-        val phoneIndex =
-            it.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
-
-        while (it.moveToNext()) {
-            contacts.add(
-                ContactNumber(
-                    name = it.getString(nameIndex),
-                    phoneNumber = it.getString(phoneIndex)
-                )
-            )
-        }
+@Composable
+fun SearchBar(contacts: List<ContactNumber>, onQueryChanged: (List<ContactNumber>) -> Unit) {
+    var text by remember { mutableStateOf("") }
+    Surface(modifier = Modifier.fillMaxWidth().statusBarsPadding(),
+        tonalElevation = 3.dp) {
+        TextField(
+            value = text,
+            onValueChange = { query ->
+                text = query
+                val filterList =
+                    contacts.filter { it.name.contains(query, ignoreCase = true) }.toMutableList()
+                onQueryChanged(filterList)
+            },
+        )
     }
-
-    return contacts
-
 }
